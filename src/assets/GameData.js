44b7ae5c6f8f08abe60c779e2f1aac6c7e5d84f9
iamkiwii <kiwiiplays@gmail.com>
2023-05-21 21:23:38 -0700

    started character page

--- src/assets/GameData.js
+++ src/assets/GameData.js
@@ -69,7 +69,216 @@ var Chuck = {
     }
 }
 
+var Anna = {
+    name: "Anna",
+    abilities: {
+        basic: {
+            name: "Run N' Gun",
+            desc: "Fire 4 smal bullets."
+        },
+        special: {
+            name: "Eat Lead",
+            desc: "Fire 3 big bullets at once. Reset CYA! on hit."
+        },
+        movement: {
+            name: "CYA!",
+            desc: "Perform a quick dash."
+        },
+        ultimate: {
+            name: "Bullet Hell",
+            desc: "Spin and shoot everything around you. Apply slow on hit."
+        }
+    }
+}
+
+var Zook = {
+    name: "Zook",
+    abilities: {
+        basic: {
+            name: "Bazooka",
+            desc: "Fire a rocket that explodes on impact."
+        },
+        special: {
+            name: "Locked On",
+            desc: "Fire a homing missile that tracks targets."
+        },
+        movement: {
+            name: "Gotta Blast",
+            desc: "Blast the ground to launch yourself to a location. Apply knock up on hit."
+        },
+        ultimate: {
+            name: "Mine your step",
+            desc: "Throw proximity mines on the ground. Apply slow on hit."
+        }
+    }
+}
+
+var Kitsu = {
+    name: "Kitsu",
+    abilities: {
+        basic: {
+            name: "Violet Strike",
+            desc: "Shoot an energy arrow."
+        },
+        special: {
+            name: "Mooncall",
+            desc: "Rain down arrows in an area. Apply slow on hit."
+        },
+        movement: {
+            name: "Foxtrot",
+            desc: "Shoot the ground to jump away. Apply knock up on hit."
+        },
+        ultimate: {
+            name: "Shooting Stars",
+            desc: "Shoot a piercing arrow through obstacles and enemies. Apply root on hit."
+        }
+    }
+}
+
+var Sashimi = {
+    name: "Sashimi",
+    abilities: {
+        basic: {
+            name: "Chop Chop",
+            desc: "Punch for a 3-hit combo."
+        },
+        special: {
+            name: "Ginger Aftertaste",
+            desc: "Clap enemies in front of you. Apply stun and weaken on hit."
+        },
+        movement: {
+            name: "Sushi Roll",
+            desc: "Charge a dash, deal damage and slow targets along the way."
+        },
+        ultimate: {
+            name: "Tempura Shock",
+            desc: "Jump and slam into an area. On hit, knock up enemies and shield yourself and allies."
+        }
+    }
+}
+
+var Kiddo = {
+    name: "Kiddo",
+    abilities: {
+        basic: {
+            name: "Light it up",
+            desc: "Light up an area."
+        },
+        special: {
+            name: "It's Lit",
+            desc: "Turn an area into an expanding fire. Apply burn and slow on hit."
+        },
+        movement: {
+            name: "Flame on",
+            desc: "Activate a fire shield."
+        },
+        ultimate: {
+            name: "Blaze it",
+            desc: "Send a huge meteorite crashing into the ground."
+        }
+    }
+}
+
+var Skinny = {
+    name: "Skinny",
+    abilities: {
+        basic: {
+            name: "Reap",
+            desc: "Slash for a 2-hit combo."
+        },
+        special: {
+            name: "Claw",
+            desc: "Slice the ground. Apply Exposed on hit."
+        },
+        movement: {
+            name: "Shadow Walk",
+            desc: "Become invisible and gain speed."
+        },
+        ultimate: {
+            name: "RIP",
+            desc: "Dash forward and slash in front of you."
+        }
+    }
+}
+
+var Froggy = {
+    name: "Froggy",
+    abilities: {
+        basic: {
+            name: "Ribbhit",
+            desc: "Slash for a 3-hit combo."
+        },
+        special: {
+            name: "MLEM",
+            desc: "Stick to a target with your tongue. Apply slow on hit."
+        },
+        movement: {
+            name: "Hip Hop",
+            desc: "Leap to a location, inflict slow when landing."
+        },
+        ultimate: {
+            name: "Slice N' Dice",
+            desc: "Dash forward and deal hgih damage to opponents along the way."
+        }
+    }
+}
+
+var Teevee = {
+    name: "Teevee",
+    abilities: {
+        basic: {
+            name: "Hello World",
+            desc: "Shoot out a digital wave."
+        },
+        special: {
+            name: "Raycast",
+            desc: "Shoot out a beam. Apply slow on hit."
+        },
+        movement: {
+            name: "Ctrl+C & Ctrl+V",
+            desc: "Teleport to a location and create a clone at your original position. Recast to swap positions with your clone."
+        },
+        ultimate: {
+            name: "memcpy",
+            desc: "Create a clone at a location. Recast to swap positions with your clone."
+        }
+    }
+}
+
+var Sofia = {
+    name: "Sofia",
+    abilities: {
+        basic: {
+            name: "Fire Slash",
+            desc: "Slash for a 3-hit combo."
+        },
+        special: {
+            name: "Reflect Stance",
+            desc: "Counters attacks and reflexts projectiles."
+        },
+        movement: {
+            name: "Fiery Dash",
+            desc: "Dash forward, setting your katana on fire. Recast upon hitting an enemy up to 3 times."
+        },
+        ultimate: {
+            name: "Flamestrike",
+            desc: "Hurl a fiery projectile with your katana. Flamestrike is charged by hitting opponents with Fire Slash."
+        }
+    }
+
+}
+
+
 GameData.champs.push(new ChampObject(Chuck))
+GameData.champs.push(new ChampObject(Anna))
+GameData.champs.push(new ChampObject(Zook))
+GameData.champs.push(new ChampObject(Kitsu))
+GameData.champs.push(new ChampObject(Sashimi))
+GameData.champs.push(new ChampObject(Kiddo))
+GameData.champs.push(new ChampObject(Skinny))
+GameData.champs.push(new ChampObject(Froggy))
+GameData.champs.push(new ChampObject(Teevee))
+GameData.champs.push(new ChampObject(Sofia))
 
 
 // Ranks Data
