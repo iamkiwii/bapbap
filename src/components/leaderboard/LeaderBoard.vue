442888e02f6193a73e7ee3033085ea9688614301
iamkiwii <kiwiiplays@gmail.com>
2023-05-24 19:09:31 -0700

    learning api

---
+++ src/components/leaderboard/LeaderBoard.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+import axios from 'axios'
+
+</script>
+
+<script>
+
+export default {
+    data() {
+        return {
+        }
+    },
+    mounted() {
+        axios({
+            method: 'get',
+            url: `https://bapbap.gg/api/leaderboards/all/7?page=0`,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+            },
+            data: null,
+            reponseType: null,
+            timeout: 0,
+            params: null
+        })
+    }
+}
+
+
+</script>
+
+
+<style lang="scss" scoped></style>
--- src/views/LeaderboardsView.vue
+++ src/views/LeaderboardsView.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-
+        <LeaderBoard />
     </div>
 </template>
 
 <script setup>
-
+import LeaderBoard from '@/components/leaderboard/leaderboard.vue'
 </script>
 
 <style lang="scss" scoped></style>
