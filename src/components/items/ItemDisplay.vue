8e3ec249a3b099cb52301a53ffe017a519c89519
iamkiwii <kiwiiplays@gmail.com>
2023-05-25 15:57:29 -0700

    start of redesigning items page completely
    summary was a lie it wasnt really a start i just deleted the item page lol

--- src/components/items/ItemDisplay.vue
+++ src/components/items/ItemDisplay.vue
@@ -1,18 +1,6 @@
 <template>
     <div class="container-wrapper"> <!-- Wraps the entire items display-->
-        <div class="item-type-wrapper">
-            <div :class="item.name">
-                <div class="circle">
-                    <img :src="getImage()" class="item" draggable="false" :class="type">
-                </div>
-                <div class="item-type">{{ item.name }}</div>
-            </div>
-        </div>
-        <div v-for="color in item.colors" :key="color" class="colors">
-            <!-- Loops through all colors and wraps each item type and all its tiers-->
-            <ItemContainer :filename="getImage()" :color="color" :desc="item.desc" :stats="item.stats" :type="activeItem"
-                :name="item.name" />
-        </div>
+
     </div>
 </template>
 
@@ -42,215 +30,35 @@ import Item_Hat_CDR from '@/assets/items/Item_Hat_CDR.png'
 <script>
 
 export default {
-    props: {
-        item: ItemObject
-    },
-    components: {
-        ItemContainer
-    },
-    methods: {
-        getImage() {
-            var filename = "Item_" + UserData.currentActive + "_" + this.item.type
-            switch (filename) {
-                case "Item_Backpack_CDR":
-                    return Item_Backpack_CDR
-                case "Item_Backpack_CRIT":
-                    return Item_Backpack_CRIT
-                case "Item_Backpack_DMG":
-                    return Item_Backpack_DMG
-                case "Item_Backpack_HP":
-                    return Item_Backpack_HP
-                case "Item_Backpack_LS":
-                    return Item_Backpack_LS
-                case "Item_Hat_CDR":
-                    return Item_Hat_CDR
-                case "Item_Hat_CRIT":
-                    return Item_Hat_CRIT
-                case "Item_Hat_DMG":
-                    return Item_Hat_DMG
-                case "Item_Hat_HP":
-                    return Item_Hat_HP
-                case "Item_Hat_LS":
-                    return Item_Hat_LS
-                case "Item_Boots_CDR":
-                    return Item_Boots_CDR
-                case "Item_Boots_CRIT":
-                    return Item_Boots_CRIT
-                case "Item_Boots_DMG":
-                    return Item_Boots_DMG
-                case "Item_Boots_HP":
-                    return Item_Boots_HP
-                case "Item_Boots_LS":
-                    return Item_Boots_LS
-                case "Item_undefined_CDR":
-                    return Item_Hat_CDR
-                case "Item_undefined_CRIT":
-                    return Item_Hat_CRIT
-                case "Item_undefined_DMG":
-                    return Item_Hat_DMG
-                case "Item_undefined_HP":
-                    return Item_Hat_HP
-                case "Item_undefined_LS":
-                    return Item_Hat_LS
-            }
-        }
-    },
-    data() {
-        return {
-            activeItem: UserData.currentActive
-        }
-    }
+
 }
 
 
 </script>
 
 <style lang="scss" scoped>
-.item-type-wrapper {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    margin-bottom: 4%;
-}
+// COLORS
 
-.item {
-    height: 50px;
-    width: 50px;
-    margin-left: 11px;
-    margin-bottom: 4px;
-}
+// VAMP
+// background-color: #6121aa;
+// box-shadow: 2px 2px 0px rgba($color: #3c1569, $alpha: 1), 5px 5px 8px rgba($color: #000000, $alpha: 0.3);
 
+// BULKY
+// background-color: #589418;
+// box-shadow: 2px 2px 0px rgba($color: #375e0e, $alpha: 1), 5px 5px 8px rgba($color: #000000, $alpha: 0.3);
 
-// Images are a bit weird, so I had to add some margins to make them look good
-.Boots {
-    margin-top: 8px;
-}
+// SHARP
+// background-color: #b2090d;
+// box-shadow: 2px 2px 0px rgba($color: #7a0608, $alpha: 1), 5px 5px 8px rgba($color: #000000, $alpha: 0.3);
 
-.Backpack {
-    margin-top: -2px;
-    margin-left: 13px !important;
-}
+// SNAP
+// background-color: #e3a020;
+// box-shadow: 2px 2px 0px rgba($color: #a06f15, $alpha: 1), 5px 5px 8px rgba($color: #000000, $alpha: 0.3);
 
-
-.container-wrapper {
-    display: flex;
-    flex-direction: column;
-}
-
-
-// Item type colors
-.Chrono {
-    width: 80%;
-    display: flex;
-    align-items: center;
-
-    .circle {
-        align-items: center;
-        display: flex;
-        width: 70px;
-        height: 70px;
-        border-radius: 50%;
-        background-color: #2dabb7;
-        box-shadow: 2px 2px 0px rgba($color: #1d7077, $alpha: 1), 5px 5px 8px rgba($color: #000000, $alpha: 0.3);
-    }
-
-    .item-type {
-        margin-left: 5%;
-        font-size: 30px;
-        color: white;
-        text-shadow: 3px 2px 0px rgba($color: #a5a5a5, $alpha: 1), 5px 5px 8px rgba($color: #000000, $alpha: 0.3);
-    }
-}
-
-.Snap {
-    width: 100%;
-    display: flex;
-    align-items: center;
-
-    .circle {
-        align-items: center;
-        display: flex;
-        width: 70px;
-        height: 70px;
-        border-radius: 50%;
-        background-color: #e3a020;
-        box-shadow: 2px 2px 0px rgba($color: #a06f15, $alpha: 1), 5px 5px 8px rgba($color: #000000, $alpha: 0.3);
-    }
-
-    .item-type {
-        margin-left: 5%;
-        font-size: 30px;
-        color: white;
-        text-shadow: 3px 2px 0px rgba($color: #a5a5a5, $alpha: 1), 5px 5px 8px rgba($color: #000000, $alpha: 0.3);
-    }
-}
-
-.Sharp {
-    width: 100%;
-    display: flex;
-    align-items: center;
-
-    .circle {
-        align-items: center;
-        display: flex;
-        width: 70px;
-        height: 70px;
-        border-radius: 50%;
-        background-color: #b2090d;
-        box-shadow: 2px 2px 0px rgba($color: #7a0608, $alpha: 1), 5px 5px 8px rgba($color: #000000, $alpha: 0.3);
-    }
-
-    .item-type {
-        margin-left: 5%;
-        font-size: 30px;
-        color: white;
-        text-shadow: 3px 2px 0px rgba($color: #a5a5a5, $alpha: 1), 5px 5px 8px rgba($color: #000000, $alpha: 0.3);
-    }
-}
-
-.Bulky {
-    width: 100%;
-    display: flex;
-    align-items: center;
-
-    .circle {
-        align-items: center;
-        display: flex;
-        width: 70px;
-        height: 70px;
-        border-radius: 50%;
-        background-color: #589418;
-        box-shadow: 2px 2px 0px rgba($color: #375e0e, $alpha: 1), 5px 5px 8px rgba($color: #000000, $alpha: 0.3);
-    }
-
-    .item-type {
-        margin-left: 5%;
-        font-size: 30px;
-        color: white;
-        text-shadow: 3px 2px 0px rgba($color: #a5a5a5, $alpha: 1), 5px 5px 8px rgba($color: #000000, $alpha: 0.3);
-    }
-}
-
-.Vamp {
-    width: 100%;
-    display: flex;
-    align-items: center;
-
-    .circle {
-        align-items: center;
-        display: flex;
-        width: 70px;
-        height: 70px;
-        border-radius: 50%;
-        background-color: #6121aa;
-        box-shadow: 2px 2px 0px rgba($color: #3c1569, $alpha: 1), 5px 5px 8px rgba($color: #000000, $alpha: 0.3);
-    }
-
-    .item-type {
-        margin-left: 5%;
-        font-size: 30px;
-        color: white;
-        text-shadow: 3px 2px 0px rgba($color: #a5a5a5, $alpha: 1), 5px 5px 8px rgba($color: #000000, $alpha: 0.3);
-    }
-}
+// CHRONO
+// background-color: #2dabb7;
+// box-shadow: 2px 2px 0px rgba($color: #1d7077, $alpha: 1), 5px 5px 8px rgba($color: #000000, $alpha: 0.3);
 </style>
+
+
+
--- src/views/ItemsView.vue
+++ src/views/ItemsView.vue
@@ -4,102 +4,22 @@
             <ItemSelector :item="ActiveItemObject" @click="toggleActiveItem(ActiveItemObject)" :key="updateKey" />
         </div>
     </div>
-
-    <div class="item-display-wrapper">
-        <div class="item-display" v-for="item in GameData.itemlist" :key="item">
-            <ItemDisplay :item="item" :key="updateKey" />
-        </div>
-    </div>
 </template>
 
 <script setup>
 import ItemSelector from '@/components/items/ItemSelector.vue';
-import ItemDisplay from '@/components/items/ItemDisplay.vue';
-import GameData from '@/assets/GameData.js'
-import UserData from '@/assets/UserData.js';
-import { ItemObject } from '@/assets/GameData.js';
-import { ActiveItemObject } from '@/assets/UserData.js';
 
 
 </script>
 
 <script>
 export default {
-    data() {
-        return {
-            GameData: GameData,
-            item: ItemObject,
-            ActiveItemObject: ActiveItemObject,
-            updateKey: 0
-        }
-    },
-    components: {
-        ItemDisplay
-    },
-    methods: {
-        // Toggles active item in UserData.ActiveItems then forces a re-render of the ItemSelector component
-        toggleActiveItem(item) {
-            toggleActive(item);
-            this.forceRender();
-        },
-        // Forces a re-render of the ItemSelector component
-        forceRender() {
-            this.updateKey++;
-        }
-    }
-
 }
 
-function toggleActive(a) {
-    UserData.currentActive = a.name
-    if (a.isActive == true) {
-        return;
-    }
-    for (let i = 0; i < UserData.ActiveItems.length; i++) {
-        if (UserData.ActiveItems[i] == a) {
-            UserData.ActiveItems[i].isActive = true;
-        } else {
-            UserData.ActiveItems[i].isActive = false;
-        }
-    }
-    a.isActive = true;
-}
 
 
 
 </script>
 
 
-<style lang="scss" scoped>
-.item-display {
-    width: 20%;
-}
-
-.item-type-banner {
-    width: 20%;
-    text-align: center;
-}
-
-.item-types-wrapper {
-    display: flex;
-    flex-direction: row;
-}
-
-.item-display-wrapper {
-    margin-top: 3%;
-    display: flex;
-    flex-direction: row;
-    transition: all 0.5s ease;
-}
-
-
-.item-selector-wrapper {
-    margin-top: 40px;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    width: fit-content;
-    height: fit-content;
-    transform: skew(-5deg);
-}
-</style>
+<style lang="scss" scoped></style>
